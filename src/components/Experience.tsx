36770912bde6e68771e7fdbb697f2b845ee664d9
Abhishek katroliya <56072322+Abhishekkat@users.noreply.github.com>
2024-12-13 13:01:05 +0530

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -16,8 +16,7 @@ const Experience = () => {
       company: "Epitome Cloud Inc.",
       period: "2021/12 - 2024/7",
       location: "Hyderabad,india",
-      description: "•
- Configure CLM software platforms, including system setup, user permissions, data migration, and integration with other business systems (e.g., CRM, ERP).",
+      description: "Configure CLM software platforms, including system setup, user permissions, data migration, and integration with other business systems (e.g., CRM, ERP).",
       icon: <GraduationCap className="h-6 w-6 text-blue-400" />
     },
     {
